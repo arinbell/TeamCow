e267126b24c0b3b3ab825dc84de5966a07c9cade
WinfinitytheStar <w.anyanwu@live.com>
2017-06-21 17:14:37 -0400

    "Homepage goes to registration page"
    The item registration page is not yet implemeted properly and the fields on the homepage and the item registration page currently overlap.

--- app/src/main/java/com/teamcow/wheresmystuff/HomepageActivity.java
+++ app/src/main/java/com/teamcow/wheresmystuff/HomepageActivity.java
@@ -1,5 +1,6 @@
 package com.teamcow.wheresmystuff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,12 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         Button homepageLogoutButton = (Button) findViewById(R.id.logout_button);
+        Button homepageItemRegButton = (Button) findViewById(R.id.homepage_item_reg_button);
+        homepageItemRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                goToItemRegister();
+            }
+        });
         homepageLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +29,10 @@ public class HomepageActivity extends AppCompatActivity {
 
     private void attemptLogout() {
         finish();
+    }
+
+    private void goToItemRegister() {
+        Intent intent = new Intent(HomepageActivity.this, ItemRegActivity.class);
+        startActivity(intent);
     }
 }
