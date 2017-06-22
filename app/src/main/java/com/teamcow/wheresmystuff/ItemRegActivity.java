2c7767c2d8c65997246ced135fac14b504805849
WinfinitytheStar <w.anyanwu@live.com>
2017-06-21 21:07:28 -0400

    "Class for storing Item Objects Created"

--- app/src/main/java/com/teamcow/wheresmystuff/ItemRegActivity.java
+++ app/src/main/java/com/teamcow/wheresmystuff/ItemRegActivity.java
@@ -2,6 +2,8 @@ package com.teamcow.wheresmystuff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ItemRegActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ItemRegActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_reg);
+        Button ItemRegButton = (Button) findViewById(R.id.item_reg_button);
+        ItemRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view) {
+                itemRegistration();
+            }
+        });
+    }
+
+    public void itemRegistration() {
+
     }
 }
---
+++ app/src/main/java/com/teamcow/wheresmystuff/LostItemDatabase.java
@@ -0,0 +1,15 @@
+package com.teamcow.wheresmystuff;
+
+import java.util.ArrayList;
+
+/**
+ * Created by WAnya on 6/21/2017.
+ */
+
+public class LostItemDatabase {
+    public static ArrayList<LostItem> itemList;
+
+    public LostItemDatabase(LostItem i) {
+        itemList.add(i);
+    }
+}
