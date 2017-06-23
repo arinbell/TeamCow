ad639ec18fe446399eee8c59f639a79a20a9c446
WinfinitytheStar <w.anyanwu@live.com>
2017-06-22 20:00:55 -0400

    "Post An Item Button sends user to Item registration page"

--- app/src/main/java/com/teamcow/wheresmystuff/controller/HomepageActivity.java
+++ app/src/main/java/com/teamcow/wheresmystuff/controller/HomepageActivity.java
@@ -1,5 +1,6 @@
 package com.teamcow.wheresmystuff.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,15 +15,27 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         Button homepageLogoutButton = (Button) findViewById(R.id.logout_button);
+        Button itemRegButton = (Button) findViewById(R.id.homepage_item_button);
+        Button itemSearchButton = (Button) findViewById(R.id.homepage_search_button);
         homepageLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogout();
             }
         });
+        itemRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view) {
+                goToItemReg();
+            }
+        });
     }
 
     private void attemptLogout() {
         finish();
+    }
+
+    private void goToItemReg() {
+        Intent intent = new Intent(HomepageActivity.this, ItemRegActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/teamcow/wheresmystuff/controller/ItemRegActivity.java
@@ -0,0 +1,15 @@
+package com.teamcow.wheresmystuff.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.teamcow.wheresmystuff.R;
+
+public class ItemRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_reg);
+    }
+}
