5b61f1f6bc39fd571de0829ad65dd1525213e2b1
lfeng42 <lian.feng97@gmail.com>
2017-06-29 16:16:41 -0400

    Created UserInfoActivity

---
+++ app/src/main/java/com/teamcow/wheresmystuff/controller/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.teamcow.wheresmystuff.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.teamcow.wheresmystuff.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
